9f48a7b1874d63277eb6455fbc357b23b0283a19
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-04-23 14:40:09 +0000

    add contribute link

--- contributions/urls.py
+++ contributions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contributions, name='contributions'),
+    path('contribute/', views.contributions, name='contribute'),
 ]
