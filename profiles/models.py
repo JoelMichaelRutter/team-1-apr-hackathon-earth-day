ac464a817654f44cb932af37715a8d4796cb26b1
John Traas <j.j.h.traas@gmail.com>
2022-04-24 22:55:59 +0000

    stashing for branch

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from PIL import Image
+
+
 class UserProfile(models.Model):
     """
     Maintain User Profile history
@@ -12,3 +15,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    # resizing images
+    # def save(self, *args, **kwargs):
+    #     super().save()
+
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         new_img = (300, 300)
+    #         img.thumbnail(new_img)
+    #         img.save(self.image.path)
