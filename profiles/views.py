760d8245a98c4e6233d49443fcedeb46a11302c1
Nom115 <noumair.rafiq@gmail.com>
2022-04-24 23:23:58 +0000

    Add account tabs and media query

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
+from contributions.models import Reuse, Reduce, Recycle
 # Create your views here.
 
 def profile(request):
     """ A view to return index page """
+    user_reuse_contributions = Reuse.objects.filter(profile=request.user)
+    user_reduce_contributions = Reduce.objects.all()
+    user_recycle_contributions = Recycle.objects.all()
+    template = 'profiles/profile.html'
+    context = {
+        'user_reuse_contributions': user_reuse_contributions,
+        'user_reduce_contributions': user_reduce_contributions,
+        'user_recycle_contributions': user_recycle_contributions,
+    }
 
-    return render(request, 'profiles/profile.html')
+
+
+    return render(request, template, context)
