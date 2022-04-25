5c91733be153440c665702821aa8c28178cea22f
John Traas <j.j.h.traas@gmail.com>
2022-04-25 00:09:31 +0000

    create images folder in static

--- earrrth/settings.py
+++ earrrth/settings.py
@@ -122,18 +122,18 @@ WSGI_APPLICATION = 'earrrth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEVELOPMENT' in os.environ:
+# if 'DEVELOPMENT' in os.environ:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
