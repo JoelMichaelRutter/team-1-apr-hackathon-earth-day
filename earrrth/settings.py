116e21d1f3a5a8b450f02dc324944fbe2ed871ef
John Traas <j.j.h.traas@gmail.com>
2022-04-25 00:35:30 +0000

    update toasts icons to use MEDIA_URL + style.css hero images urls

--- earrrth/settings.py
+++ earrrth/settings.py
@@ -179,7 +179,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'  # noqa: E501
 CLOUDINARY_STORAGE = {
